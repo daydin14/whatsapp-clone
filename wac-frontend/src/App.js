111fcf564ad49971846d5d7b5178f09f4f34c962
David <david.aydin6@gmail.com>
2023-10-23 17:01:27 -0400

    create-react-app & vscode color theme

---
+++ wac-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a MERN WhatsApp Clone</h1>
+
+      {/* Sidebar */}
+      {/* Chat Component */}
+    </div>
+  );
+}
+
+export default App;
