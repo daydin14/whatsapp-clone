658a71e8b119c292260fc7e077b280e326b37716
David <david.aydin6@gmail.com>
2023-10-24 18:53:05 -0400

    Material UI

--- wac-frontend/src/index.js
+++ wac-frontend/src/index.js
@@ -5,6 +5,12 @@ import ReactDOM from 'react-dom/client';
 // Styling
 import './index.css';
 
+// Material UI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 // Components
 import App from './App';
 import reportWebVitals from './reportWebVitals';
