f78092ca8208efa8940bbede49e5e2b047952e7d
David <david.aydin6@gmail.com>
2023-10-30 22:44:21 -0400

    Chat text input bar functionality

--- wac-frontend/src/components/Chat/ChatFooter/ChatFooter.js
+++ wac-frontend/src/components/Chat/ChatFooter/ChatFooter.js
@@ -1,17 +1,36 @@
 // Dependencies
-import React from 'react'
+import React, { useState } from 'react'
 
 // Components
 import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon'
 import MicIcon from '@mui/icons-material/Mic'
+import axios from '../../../axios';
 
 const ChatFooter = () => {
+
+    const [input, setInput] = useState("");
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        await axios.post('/messages/new', {
+            message: input,
+            name: "Dev Guy",
+            timestampe: "Now!",
+            received: false,
+        });
+
+        setInput('');
+    }
+
     return (
         <div className='chat__footer'>
             <InsertEmoticonIcon />
             <form>
-                <input placeholder='Type a message' type='text' />
-                <button type='submit'>
+                <input
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                    placeholder='Type a message'
+                    type='text' />
+                <button type='submit' onClick={sendMessage}>
                     Send a message
                 </button>
             </form>
